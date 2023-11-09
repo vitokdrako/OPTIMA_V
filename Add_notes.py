f5835ba1c53c9e4bbc4de9b63599d643d456b14b
Crsd <does.it.terrify.you@gmail.com>
2023-11-09 20:55:49 +0200

    changes

--- Add_notes.py
+++ Add_notes.py
@@ -36,29 +36,40 @@ class Note:
         self._text = new_text
         self._parse_tags()
 
-    class NotesList(UserList):
-        def __init__(self):
-            super().__init__()
-            self.filename = "notes.pkl"  # Назва файлу для збереження нотаток
+class NotesList(UserList):
+    def __init__(self):
+        super().__init__()
+        self.filename = "notes.pkl"  # Назва файлу для збереження нотаток
 
         # Перевизначення методу append для автоматичного збереження при додаванні нотаток
-        def append(self, note):
-            super().append(note)
-            self._save_notes_to_file()
+    def append(self, note):
+        super().append(note)
+        self._save_notes_to_file()
 
         # Збереження нотаток у файл
-        def _save_notes_to_file(self):
-            with open(self.filename, 'wb') as file:
-                pickle.dump(self.data, file)
+    def _save_notes_to_file(self):
+        with open(self.filename, 'wb') as file:
+            pickle.dump(self.data, file)
 
         # Завантаження нотаток з файлу
-        def load_notes_from_file(self):
-            try:
-                with open(self.filename, 'rb') as file:
-                    self.data = pickle.load(file)
-            except (FileNotFoundError, EOFError):
-                self.data = []
+    def load_notes_from_file(self):
+        try:
+            with open(self.filename, 'rb') as file:
+                self.data = pickle.load(file)
+        except (FileNotFoundError, EOFError):
+            self.data = []
 
     # Код для збереження та завантаження нотаток
-    notes_list = NotesList()
-    notes_list.load_notes_from_file()
+notes_list = NotesList()
+notes_list.load_notes_from_file()
+
+note1 = Note("Title 1", "Text of Note 1 #tag1 #tag2")
+note2 = Note("Title 2", "Text of Note 2 #tag2 #tag3")
+notes_list.append(note1)  # Автоматичне збереження при додаванні
+notes_list.append(note2)
+
+# Виведення нотаток
+for note in notes_list:
+    print(f"Title: {note.title}")
+    print(f"Text: {note.text}")
+    print(f"Tags: {note.tags_dict}")
