b9b1eb368440030186f044e3bf6890a92cae7b51
Crsd <does.it.terrify.you@gmail.com>
2023-11-09 14:46:54 +0200

    add notes module (save and load)

---
+++ Add_notes.py
@@ -0,0 +1,64 @@
+import re
+from collections import UserList
+import pickle
+
+
+class Note:
+    def __init__(self, title, text):
+        self._title = title
+        self._text = text
+        self._tags_dict = {}
+        self._parse_tags()
+
+    @property
+    def title(self):
+        return self._title
+
+    @property
+    def text(self):
+        return self._text
+
+    @property
+    def tags_dict(self):
+        return self._tags_dict
+
+    def _parse_tags(self):
+        # Регулярний вираз для пошуку хештегів в тексті
+        pattern = r"#\w+"
+        tags = re.findall(pattern, self._text)
+
+        for tag in tags:
+            tag = tag[1:]  # Видаляємо символ "#" з тегу
+            self._tags_dict[tag] = self._tags_dict.get(tag, 0) + 1
+
+    @text.setter
+    def text(self, new_text):
+        self._text = new_text
+        self._parse_tags()
+
+    class NotesList(UserList):
+        def __init__(self):
+            super().__init__()
+            self.filename = "notes.pkl"  # Назва файлу для збереження нотаток
+
+        # Перевизначення методу append для автоматичного збереження при додаванні нотаток
+        def append(self, note):
+            super().append(note)
+            self._save_notes_to_file()
+
+        # Збереження нотаток у файл
+        def _save_notes_to_file(self):
+            with open(self.filename, 'wb') as file:
+                pickle.dump(self.data, file)
+
+        # Завантаження нотаток з файлу
+        def load_notes_from_file(self):
+            try:
+                with open(self.filename, 'rb') as file:
+                    self.data = pickle.load(file)
+            except (FileNotFoundError, EOFError):
+                self.data = []
+
+    # Код для збереження та завантаження нотаток
+    notes_list = NotesList()
+    notes_list.load_notes_from_file()
