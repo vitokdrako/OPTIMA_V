961c69889a891ad54b6bcafe44a76b7b787e4175
Crsd <does.it.terrify.you@gmail.com>
2023-11-10 14:41:05 +0200

    clearing code

--- Add_notes.py
+++ Add_notes.py
@@ -1,4 +1,4 @@
-import re
+# import re
 from collections import UserList
 import pickle
 
@@ -22,54 +22,40 @@ class Note:
     def tags_dict(self):
         return self._tags_dict
 
-    def _parse_tags(self):
-        # Регулярний вираз для пошуку хештегів в тексті
-        pattern = r"#\w+"
-        tags = re.findall(pattern, self._text)
-
-        for tag in tags:
-            tag = tag[1:]  # Видаляємо символ "#" з тегу
-            self._tags_dict[tag] = self._tags_dict.get(tag, 0) + 1
+    # irrelevant kode:
+    # def _parse_tags(self):
+    #     pattern = r"#\w+"
+    #     tags = re.findall(pattern, self._text)
+    #
+    #     for tag in tags:
+    #         tag = tag[1:]
+    #         self._tags_dict[tag] = self._tags_dict.get(tag, 0) + 1
 
     @text.setter
     def text(self, new_text):
         self._text = new_text
         self._parse_tags()
 
+    def _parse_tags(self):
+        pass
+
+
 class NotesList(UserList):
     def __init__(self):
         super().__init__()
-        self.filename = "notes.pkl"  # Назва файлу для збереження нотаток
+        self.filename = "notes.pkl"
 
-        # Перевизначення методу append для автоматичного збереження при додаванні нотаток
     def append(self, note):
         super().append(note)
         self._save_notes_to_file()
 
-        # Збереження нотаток у файл
     def _save_notes_to_file(self):
         with open(self.filename, 'wb') as file:
             pickle.dump(self.data, file)
 
-        # Завантаження нотаток з файлу
     def load_notes_from_file(self):
         try:
             with open(self.filename, 'rb') as file:
                 self.data = pickle.load(file)
         except (FileNotFoundError, EOFError):
             self.data = []
-
-    # Код для збереження та завантаження нотаток
-notes_list = NotesList()
-notes_list.load_notes_from_file()
-
-note1 = Note("Title 1", "Text of Note 1 #tag1 #tag2")
-note2 = Note("Title 2", "Text of Note 2 #tag2 #tag3")
-notes_list.append(note1)  # Автоматичне збереження при додаванні
-notes_list.append(note2)
-
-# Виведення нотаток
-for note in notes_list:
-    print(f"Title: {note.title}")
-    print(f"Text: {note.text}")
-    print(f"Tags: {note.tags_dict}")
