63fe40c379f78bee1334dade662c85adf0f6bade
Anastasia-Makarova <140186425+Anastasia-Makarova@users.noreply.github.com>
2023-11-11 00:03:33 +0100

    dict of tags, edit whole note, delete note

--- Add_notes.py
+++ Add_notes.py
@@ -1,61 +1,106 @@
-# import re
+"""
+- Під капотом тегів - словник, де ключ - текст тегу, в видрук іде тільки перелік ключів
+- Теги лишаються в тексті нотатки, але дублюються в поле тегів без знаку #
+- Повернула функцію def _parse_tags та оголошення notes_list, бо без нього не працює корректно
+"""
+
+
+import re
 from collections import UserList
 import pickle
 
 
 class Note:
-    def __init__(self, title, text):
+    def __init__(self, title: str, text: str) -> None:
         self._title = title
         self._text = text
         self._tags_dict = {}
         self._parse_tags()
 
+    def __str__(self) -> str:
+        num = notes_list.index(note)+1
+        tags = ', '.join(tag for tag in self._tags_dict.keys())
+        
+        return ("{:<5} {:<20} {:<20} {:<50}".format(num, note.title, tags, note.text))
+
     @property
-    def title(self):
+    def title(self) -> str:
         return self._title
 
     @property
-    def text(self):
+    def text(self)  -> str:
         return self._text
 
     @property
-    def tags_dict(self):
+    def tags_dict(self) -> dict:
         return self._tags_dict
 
-    # irrelevant kode:
-    # def _parse_tags(self):
-    #     pattern = r"#\w+"
-    #     tags = re.findall(pattern, self._text)
-    #
-    #     for tag in tags:
-    #         tag = tag[1:]
-    #         self._tags_dict[tag] = self._tags_dict.get(tag, 0) + 1
+    def _parse_tags(self) -> None:
+        pattern = r"#\w+"
+        tags = re.findall(pattern, self._text)
+    
+        for tag in tags:
+            tag = tag[1:]
+            self._tags_dict[tag] = self._tags_dict.get(tag, 0) + 1
+
+
+
 
     @text.setter
-    def text(self, new_text):
+    def text(self, new_text: str) -> None:
         self._text = new_text
         self._parse_tags()
 
-    def _parse_tags(self):
-        pass
-
 
 class NotesList(UserList):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
-        self.filename = "notes.pkl"
+        self.filename = "notes.bin"
 
-    def append(self, note):
+    def append(self, note) -> None:
         super().append(note)
         self._save_notes_to_file()
 
-    def _save_notes_to_file(self):
+    def remove(self, num: int) -> None:
+        notes_list.pop(num-1)
+        self._save_notes_to_file()
+
+    def edit(self, num: int, title: str, text: str) -> None:
+    
+        note = Note(title, text)
+        notes_list[num-1] = note
+        self._save_notes_to_file()
+
+    def _save_notes_to_file(self) -> None:
         with open(self.filename, 'wb') as file:
             pickle.dump(self.data, file)
 
-    def load_notes_from_file(self):
+    def load_notes_from_file(self) -> None:
         try:
             with open(self.filename, 'rb') as file:
                 self.data = pickle.load(file)
         except (FileNotFoundError, EOFError):
             self.data = []
+
+notes_list = NotesList()
+notes_list.load_notes_from_file()
+
+# Test commands
+
+# note1 = Note("Birthday Party", "John's #birdhday party on Friday")
+# notes_list.append(note1)  # Автоматичне збереження при додаванні
+
+# note2 = Note("Purchase list", "Potatoes, bread, milk #buy #food")
+# notes_list.append(note2)
+
+# note3 = Note("Bill's B_day", "Dec,12 #birthday #buy_gift")
+# notes_list.append(note3)
+
+# notes_list.remove(4)     # видалення нотатки за номером
+
+# notes_list.edit(4, "Mark's birthday", "#buy gift for Mark's #birthday")
+
+
+print("{:^5} {:<20} {:<20} {:<50}".format("num", "title", "tags", "text"))
+for note in notes_list:
+    print(note)
