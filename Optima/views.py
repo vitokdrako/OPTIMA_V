d618d544b3a49c1ffc03b47ed92ae7489cc780b5
Vitokdrako <vitokdrako@gmail.com>
2023-11-26 21:12:07 +0100

    abstract class

---
+++ Optima/views.py
@@ -0,0 +1,14 @@
+from abc import ABC, abstractmethod
+
+class BaseView(ABC):
+    @abstractmethod
+    def display_contacts(self, contacts):
+        pass
+
+    @abstractmethod
+    def display_notes(self, notes):
+        pass
+
+    @abstractmethod
+    def display_commands(self, commands):
+        pass
