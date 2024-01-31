fd4bbbe7dfb8e94b95c8f022b7a10d3f2b12bf91
vitokdrako <97182717+vitokdrako@users.noreply.github.com>
2024-01-31 19:29:54 +0100

    Update views.py

--- Optima/views.py
+++ Optima/views.py
@@ -1,14 +1,34 @@
 from abc import ABC, abstractmethod
 
-class BaseView(ABC):
+class View(ABC):
     @abstractmethod
-    def display_contacts(self, contacts):
+    def display(self, data):
         pass
 
-    @abstractmethod
-    def display_notes(self, notes):
-        pass
+class ConsoleContactView(View):
+    def display(self, data):
+        for contact in data:
+            print(f"Ім'я: {contact.name}, Телефон: {contact.phone}, Електронна пошта: {contact.email}")
 
-    @abstractmethod
-    def display_commands(self, commands):
-        pass
+class ConsoleNotesView(View):
+    def display(self, data):
+        for note in data:
+            print(f"Нотатка: {note.content}, Дата: {note.date}")
+
+class ConsoleCommandInfoView(View):
+    def display(self, data):
+        print("Доступні команди:")
+        for command in data:
+            print(f"Команда: {command.name}, Опис: {command.description}")
+
+def show_contacts(contacts):
+    view = ConsoleContactView()
+    view.display(contacts)
+
+def show_notes(notes):
+    view = ConsoleNotesView()
+    view.display(notes)
+
+def show_commands(commands):
+    view = ConsoleCommandInfoView()
+    view.display(commands)
